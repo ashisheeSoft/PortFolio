c9e6aaf8d032fe7d5585320fd97fefb39fb99665
ashisheeSoft <ashishsharmaen1014@email.com>
2022-02-05 20:41:55 +0530

    contact implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './component/About'
 import Navbar from './component/Navbar';
 import Projects from './component/Projects';
 import Skills from './component/Skills'
+import Contact from './component/Contacts';
 
 function App() {
   return (
